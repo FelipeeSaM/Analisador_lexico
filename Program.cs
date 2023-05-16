a61405705ea4779c9144491e92a79a11e700bb1f
eu mesmo <luiz_sena@hotmail.com>
2023-05-15 21:29:41 -0300

    Fix: titulo rsrsrsrs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ namespace Analisador_lexico {
     class Program {
         static void Main(string[] args) {
             Analisador palavra = new Analisador();
+            Console.Title = "É NOIS, PROFESSOR UHUUUUUUUU";
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine($"Entradas começadas com números serão consideradas palavras reservadas pelo sistema");
             Console.ForegroundColor = ConsoleColor.White;
