48422849f0c9c332e54b5057dc3ba7f0bc8c6edc
eu mesmo <luiz_sena@hotmail.com>
2023-05-16 21:48:00 -0300

    Revertendo para o da matematica

--- Objeto/Analisador.cs
+++ Objeto/Analisador.cs
@@ -20,17 +20,12 @@ namespace Analisador_lexico.Objeto {
         #endregion
 
         #region parte_1_exercicio
-        private Regex caracteresPermitidos2 = new Regex("[a-su-vA-SU-V0-9â-ýÂ-Ý]");
-        private Regex caracteresPermitidos = new Regex(@"^[a-zA-Z0-9]+$");
+        private Regex caracteresPermitidos = new Regex("[a-su-vA-SU-V0-9â-ýÂ-Ý]");
         #endregion
 
         #region parte_2_exercicio
         private List<string> tokensAtomicosLetras = new List<string>(new string[] {"x", "y", "z", "w", "t" });
         private List<string> tokensAtomicosCaracteresEspeciais = new List<string>(new string[] {"+", "-", "*", "/", ",", "@", "#", "!", "(", ")", "[", "]", "{", "}" });
-        private List<string> testarPrimeiroCaractere = new List<string>(new string[] { "j", "w", "k", "y", "ç", "h", "q", "J", "W", "K", "Y", "Ç", "H", "Q" });
-        private string vogais = @"[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z][aeiouAEIOU]";
-
-
         private int contador = 0;
         #endregion
 
@@ -38,38 +33,17 @@ namespace Analisador_lexico.Objeto {
 
 
         public string AnalisarPalavra(string token) {
-            TestaPrimeiroCaractere(token);
-            TestarVogaisEconsoantes(token);
-            TestaNumeroDeTokens(token);
+            //Console.WriteLine($"A palavra foi {token}\n");
+            MisturarCaracteresEanalisa(token);
             return "";
 
         }
 
-        private void TestarVogaisEconsoantes(string tokenReservado) {
-            bool testaConsoanteEvogal = Regex.IsMatch(tokenReservado, vogais);
-            if(!testaConsoanteEvogal) {
-                Console.WriteLine("A entrada deve conter uma consoante seguida de uma vogal.");
-                Console.ReadKey();
-            }
-        }
-
-        private void TestaPrimeiroCaractere(string tokenReservado) {
-            foreach(string primeiroCaractere in testarPrimeiroCaractere) {
-                if (tokenReservado.Substring(0, 1).Contains(primeiroCaractere) ||
-                    tokenReservado.Substring(0, 1).Contains("z")               ||
-                    tokenReservado.Substring(0, 1).Contains("x")) {
-                    Console.WriteLine("Palavra reservada pelo sistema!");
-                    Console.ReadKey();
-                    Environment.Exit(1);
-                }
-            }
-        }
-
-        private void TestaNumeroDeTokens(string tokenReservado) {
+        private void MisturarCaracteresEanalisa(string tokenReservado) {
             int validaExpressaoMatematica = AnalisarTokensEspeciais(tokenReservado);
 
-            if (validaExpressaoMatematica > 10) {
-                Console.WriteLine("Permitido no máximo 10 tokens atômicos.");
+            if (validaExpressaoMatematica >= 1) {
+                Console.WriteLine("Possível expressão matemática");
                 Console.ReadKey();
             } else {
                 AnalisarCaracteresMinusculos(tokenReservado);
@@ -93,12 +67,13 @@ namespace Analisador_lexico.Objeto {
                 tokensAchados += $"{match.Value}";
             }
 
+            //Console.Write($"Achei: {tokensAchados} \n");
+
             if(tokenReservado.Length != tokensAchados.Length) {
                 Console.WriteLine("Entrada inválida");
                 Console.ReadKey();
             } else {
                 Console.WriteLine("Entrada válida");
-                Console.ReadKey();
             }
         }
 
--- Program.cs
+++ Program.cs
@@ -5,10 +5,6 @@ namespace Analisador_lexico {
     class Program {
         static void Main(string[] args) {
             Analisador palavra = new Analisador();
-            Console.Title = "É NOIS, PROFESSOR UHUUUUUUUU";
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"Entradas começadas com números serão consideradas palavras reservadas pelo sistema");
-            Console.ForegroundColor = ConsoleColor.White;
             Console.Write("Digite o negócio: ");
             palavra.AnalisarPalavra(Console.ReadLine());
             Console.ReadKey();
